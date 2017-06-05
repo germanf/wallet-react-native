7453830b4cbdd7d130ee74ff47f4d6c9e28b13b9
BS-134\BS134 <tarekmahmud@brainstation-23.com>
2017-06-05 13:49:31 +0600

    add drawerbutton component

---
+++ app/components/drawerButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const DrawerButton = ({ navigation }) => (
+  <TouchableOpacity style={{ padding: 10 }}>
+    <Icon
+      name="menu"
+      size={35}
+      color="white"
+      onPress={() => navigation.navigate('DrawerOpen')}
+    />
+  </TouchableOpacity>
+);
+
+DrawerButton.propTypes = {
+  navigation: React.PropTypes.object.isRequired,
+};
+
+export default DrawerButton
